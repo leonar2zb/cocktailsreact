1d8cb50464a9d06218dd7f771c02e623e2393ff5
Leo en laptop <leonar2zb@gmail.com>
2025-01-13 18:39:06 -0300

    Preparando el uso de múltiples stores

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,4 +1,7 @@
+import { useAppStore } from "../stores/useAppStore"
+
 export default function IndexPage() {
+    const { categories } = useAppStore()
     return (
         <>
             Página de inicio
