fe010d5f06befba23e67651281098a81928cecba
Leo en laptop <leonar2zb@gmail.com>
2025-01-15 19:40:52 -0300

    Creando el slice para el storage para notificaciones

---
+++ src/stores/notificationSlices.ts
@@ -0,0 +1,9 @@
+import { StateCreator } from "zustand";
+
+export type NotificationSliceType = {
+
+}
+
+export const createNotificationsSlice: StateCreator<NotificationSliceType> = (set, get) => ({
+
+})
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,8 +2,12 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 import { FavoritesSliceType, createFavoritesSlice } from "./favoritesSlice";
+import { NotificationSliceType, createNotificationsSlice } from "./notificationSlices";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a),
-    ...createFavoritesSlice(...a)
-})))
+export const useAppStore = create<RecipesSliceType &
+    FavoritesSliceType &
+    NotificationSliceType>()(devtools((...a) => ({
+        ...createRecipesSlice(...a),
+        ...createFavoritesSlice(...a),
+        ...createNotificationsSlice(...a)
+    })))
