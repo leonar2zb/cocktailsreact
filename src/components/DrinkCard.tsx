0b92dc7e432664b5aae1f9ea3720735998195cb9
Leo en laptop <leonar2zb@gmail.com>
2025-01-15 09:20:05 -0300

    Mejorando diseño y aplicando estilo a las cards

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -6,8 +6,22 @@ type drinkProp = {
 
 export default function DrinkCard({ drink }: drinkProp) {
     return (
-        <div>
-            {drink.strDrink}
+        <div className="border shadow-lg">
+            <div className="overflow-hidden">
+                <img src={drink.strDrinkThumb} alt={`Imagen de ${drink.strDrink}`}
+                    className="hover:scale-125 transition-transform hover:rotate-2"
+                />
+            </div>
+            <div className="p-5">
+                <h2 className="text-2xl truncate font-black">
+                    {drink.strDrink}
+                </h2>
+                <button type="button"
+                    className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg">
+                    Ver receta
+                </button>
+
+            </div>
         </div>
     )
 }
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -7,17 +7,17 @@ export default function IndexPage() {
     const hasDrinks = useMemo(() => drinks.drinks.length, [drinks])
     return (
         <>
-            <h1>Recetas</h1>
-            {
-                hasDrinks ? (
-                    drinks.drinks.map(drink => (
+            <h1 className="text-6xl font-extrabold">Recetas</h1>
+            {hasDrinks ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+                    {drinks.drinks.map(drink => (
                         <DrinkCard key={drink.idDrink} drink={drink} />
-                    )
-                    )) : (
-                    <p>No hay resultados aún. Use el formulario para buscar recetas</p>
-                )
-            }
-
+                    ))}
+                </div>
+            ) : (
+                <p className="my-10 text-center text-2xl">
+                    No hay resultados aún. Use el formulario para buscar recetas</p>
+            )}
         </>
     )
 }
