dcada5289ec9c214e1e1a9ada1194ff64f254418
Leo en laptop <leonar2zb@gmail.com>
2025-01-15 20:06:56 -0300

    Nuevo componente Notification

--- src/stores/notificationSlices.ts
+++ src/stores/notificationSlices.ts
@@ -1,9 +1,18 @@
 import { StateCreator } from "zustand";
 
+type Notification = {
+    text: string
+    error: boolean
+    show: boolean
+}
 export type NotificationSliceType = {
-
+    notification: Notification
 }
 
 export const createNotificationsSlice: StateCreator<NotificationSliceType> = (set, get) => ({
-
+    notification: {
+        text: '',
+        error: false,
+        show: false
+    }
 })
