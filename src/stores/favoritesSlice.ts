74b89a80d14c096c29802dd845f8e90a8ae7481f
Leo en laptop <leonar2zb@gmail.com>
2025-01-15 19:04:31 -0300

    Almacenar en LocalStorage y cambiado implementación del handleFavorites

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -4,21 +4,32 @@ import { RecipeType } from "../types";
 export type FavoritesSliceType = {
     favorites: RecipeType[],
     handleClickFavorite: (recipe: RecipeType) => void,
-    hasReceipe: (id: RecipeType["idDrink"]) => boolean
+    hasReceipe: (id: RecipeType["idDrink"]) => boolean,
+    loadFromStorage: () => void
 }
 
 export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
     favorites: [],
     handleClickFavorite: (recipe) => {
         const favoritesBefore = get().favorites
+        let newFavorites = []
         const exists: boolean = favoritesBefore.some(favorite => favorite.idDrink === recipe.idDrink)
         if (exists)
-            set({
-                favorites: favoritesBefore.filter(favorite => favorite.idDrink !== recipe.idDrink)
-            })
+            newFavorites = favoritesBefore.filter(favorite => favorite.idDrink !== recipe.idDrink)
         else
-            set({ favorites: [...favoritesBefore, recipe] })
+            newFavorites = [...favoritesBefore, recipe]
+        set({
+            favorites: newFavorites
+        })
+        localStorage.setItem('favorites', JSON.stringify(newFavorites))
         console.log((exists ? 'Quitado ' : 'Agregado '), recipe.strDrink)
     },
-    hasReceipe: (id) => get().favorites.some(favorite => favorite.idDrink === id)
+    hasReceipe: (id) => get().favorites.some(favorite => favorite.idDrink === id),
+    loadFromStorage: () => {
+        const storedFavorites = localStorage.getItem('favorites')
+        if (storedFavorites)
+            set({
+                favorites: JSON.parse(storedFavorites)
+            })
+    }
 })
